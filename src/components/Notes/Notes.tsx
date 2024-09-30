9af9ccba69d9e457f6726e9888239d0e42b8e058
DeiNas77 <deinas.44Remastered@gmail.com>
2024-09-30 11:55:34 -0500

    [feature]: adding note section

---
+++ src/components/Notes/Notes.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+function Notes({}: Props) {
+  return (
+    <section className="">
+      <input type="text" placeholder="Crear una nota..." className="border-2 border-black"/>
+   
+    </section>
+  );
+}
+
+export default Notes;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import { ReactElement } from "react";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
+import SideBar from "../components/SideBar/SideBar";
+import Notes from "../components/Notes/Notes";
 
 function Home(): ReactElement | null {
   return (
     <>
       <Header />
+      <section className="flex">
+        <SideBar />
+      <div className="w-[100vw] flex justify-center">
+
+        <Notes />
+      </div>
+
+       
+      </section>
     </>
   );
 }
